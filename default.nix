5f907c55dd026c2900ee979658a546605fff7e63
mtrsk <marcos.schonfinkel@gmail.com>
2019-06-03 12:25:12 -0300

    First Commit

---
+++ default.nix
@@ -0,0 +1,25 @@
+{ nixpkgs ? import <nixpkgs> {}
+, coq ? nixpkgs.coq
+, ocaml ? nixpkgs.ocaml
+}:
+
+with nixpkgs;
+
+stdenv.mkDerivation {
+  inherit coq;
+  inherit ocaml;
+
+  name = "coq-env";
+
+  buildInputs = [
+    coq
+  ] ++ (with coq.ocamlPackages; [
+    ocaml
+    camlp5
+    findlib
+  ]) ++ (with coqPackages; [
+    coq-ext-lib
+    QuickChick
+    ssreflect
+  ]);
+}
