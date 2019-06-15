a308b2dcd8161daf17a216582bc92a64cce3115d
mtrsk <marcos.schonfinkel@gmail.com>
2019-06-15 17:26:05 -0300

    TODO: finishi parts function

--- src/Utils.v
+++ src/Utils.v
@@ -5,15 +5,11 @@ Fixpoint split {X : Type} (l : list X) : list (list X * list X) :=
   match l with
     | [] => [([], [])]
     | c::cs =>
-      let fix split' c cs :=
-          match cs with
-            | [] => []
-            | s1::s2 => (c++[s1], s2) :: split' (c++[s1]) s2
-          end
-      in
-      ([], c :: cs) :: ([c], cs) :: split' [c] cs
+      ([], c::cs) :: map (fun '(s1, s2) => (c :: s1, s2)) (split cs)
   end.
 
-Compute (split [1; 2; 3; 4]).
-Compute (split [1]).
-Compute (split []).
+Fixpoint parts {X : Type} (l : list X) : list (list (list X)) :=
+  match l with
+    | [] => [[]]
+    | (x::xs) => parts_l x xs ++ parts_r x xs
+  end.
