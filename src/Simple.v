4c2672c3d78c4944524aec1e28a021cb51457391
mtrsk <marcos.schonfinkel@gmail.com>
2019-06-14 21:32:06 -0300

    Changed Prop to Bool

--- src/Simple.v
+++ src/Simple.v
@@ -4,6 +4,8 @@ Import ListNotations.
 Require Import Bool Ascii String.
 Open Scope string_scope.
 
+Require Import Utils.
+
 Inductive regex : Type :=
   | Eps : regex
   | Sym : ascii -> regex
@@ -11,26 +13,29 @@ Inductive regex : Type :=
   | Seq : regex -> regex -> regex
   | Rep : regex -> regex.
 
-Inductive regex_match : string -> regex -> Prop :=
-  | MEps : regex_match "" Eps
-  | MSym : forall (c : ascii),
-      regex_match (String c "") (Sym c)
-  | MAltL : forall (s : string) (r1 r2 : regex),
-      regex_match s r1 -> regex_match s (Alt r1 r2)
-  | MAltR : forall (s : string) (r1 r2 : regex),
-      regex_match s r2 -> regex_match s (Alt r1 r2)
-  | MSeq : forall (s1 s2 : string) (r1 r2 : regex),
-      regex_match s1 r1 ->
-      regex_match s2 r2 ->
-      regex_match (s1 ++ s2) (Seq r1 r2)
-  | MRep0 : forall (r : regex), regex_match "" (Rep r)
-  | MRep1 : forall (s1 s2 : string) (r : regex),
-      regex_match s1 r ->
-      regex_match s2 (Rep r) ->
-      regex_match (s1++s2) (Rep r).
+Definition eq_ascii (a1 a2 : ascii) :=
+  match a1, a2 with
+  | Ascii b1 b2 b3 b4 b5 b6 b7 b8, Ascii c1 c2 c3 c4 c5 c6 c7 c8 =>
+    (eqb b1 c1) && (eqb b2 c2) && (eqb b3 c3) && (eqb b4 c4) &&
+    (eqb b5 c5) && (eqb b6 c6) && (eqb b7 c7) && (eqb b8 c8)
+  end.
 
-Notation "s ~= re" := (regex_match s re) (at level 80).
+Fixpoint eq_string (s1 s2 : string) :=
+  match s1, s2 with
+  | EmptyString,  EmptyString  => true
+  | String x1 s1, String x2 s2 => eq_ascii x1 x2 && eq_string s1 s2
+  | _, _                       => false
+  end.
 
-Compute ("a" ~= Sym "a").
-Compute ("b" ~= Alt (Sym "a") (Sym "b")).
-Check ("b" ~= Alt (Sym "a") (Sym "b")).
+Fixpoint accept (r : regex) (s : string) : bool :=
+  match r, s with
+    | Eps, "" => true
+    | Sym c, s => eq_string (String c "") s
+    | Alt r1 r2, s => accept r1 s || accept r2 s
+    | _,_ =>  false
+  end.
+
+Compute (accept Eps "a").
+Compute (accept (Sym "a") "a").
+Compute (accept (Sym "a") "b").
+Compute (accept (Alt (Sym "a") (Sym "b")) "b").
