e9a6b173e8f458b7c29d9ebc2bb3b7748a8aabfa
mtrsk <marcos.schonfinkel@gmail.com>
2019-06-16 14:51:33 -0300

    More Monoids

--- src/Monoid.v
+++ src/Monoid.v
@@ -1,4 +1,4 @@
-Require Import Coq.Bool.Bool.
+Require Import Coq.Bool.Bool Coq.Arith.PeanoNat.
 
 Class Monoid (X : Type) :=
   {
@@ -12,7 +12,7 @@ Class Monoid (X : Type) :=
     r_neutral : forall (x : X), append x empty = x;
   }.
 
-Instance BoolMonoid : Monoid bool :=
+Instance BoolMonoidOr : Monoid bool :=
   {
     empty := false;
     append x y := x || y;
@@ -26,3 +26,50 @@ Instance BoolMonoid : Monoid bool :=
   (* r_neutral *)
   Proof.
     intros. apply orb_false_r.
+
+Instance BoolMonoidAnd : Monoid bool :=
+  {
+    empty := true;
+    append x y := x && y;
+  }.
+  (* associativity *)
+  Proof.
+    intros. apply andb_assoc.
+  (* l_neutral *)
+  Proof.
+    intros. apply andb_true_l.
+  (* r_neutral *)
+  Proof.
+    intros. apply andb_true_r.
+
+Instance NatMonoidPlus : Monoid nat :=
+  {
+    empty := 0;
+    append := Nat.add;
+  }.
+  (* associativity *)
+  Proof.
+    intros. apply Nat.add_assoc.
+  (* l_neutral *)
+  Proof.
+    intros. simpl. reflexivity.
+  (* r_neutral *)
+  Proof.
+    intros.
+    rewrite <- plus_n_O.
+    reflexivity.
+
+Instance NatMonoidMul : Monoid nat :=
+  {
+    empty := 1;
+    append := Nat.mul;
+  }.
+  (* associativity *)
+  Proof.
+    intros. apply Nat.mul_assoc.
+  (* l_neutral *)
+  Proof.
+    intros. apply Nat.mul_1_l.
+  (* r_neutral *)
+  Proof.
+    intros. apply Nat.mul_1_r.
