fbec27903a1f8631f23f4b734e1d6c20997de9cf
mtrsk <marcos.schonfinkel@gmail.com>
2019-06-16 01:06:06 -0300

    TODO: finish parts from Act-1

--- src/Utils.v
+++ src/Utils.v
@@ -8,8 +8,21 @@ Fixpoint split {X : Type} (l : list X) : list (list X * list X) :=
       ([], c::cs) :: map (fun '(s1, s2) => (c :: s1, s2)) (split cs)
   end.
 
-Fixpoint parts {X : Type} (l : list X) : list (list (list X)) :=
+(* [[c] : p | p <- parts cs] *)
+
+Fixpoint parts_l {X : Type} (l : list X) : list (list (list X)) :=
   match l with
     | [] => [[]]
-    | (x::xs) => parts_l x xs ++ parts_r x xs
+    | (x::xs) => map (fun p => [x] :: p) (parts_l xs)
   end.
+
+Compute parts_l [1; 2; 3].
+
+(* [(c : p) : ps | (p:ps) <- parts' cs] *)
+
+Fixpoint parts_r {X : Type} (l : list X) : list (list (list X)) :=
+  match l with
+    | [] => [[]]
+    | (x::xs) => [[]]
+  end.
+
