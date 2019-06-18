9d6fa9b7ef5e5604619583c1fecf48294b3558e0
mtrsk <marcos.schonfinkel@gmail.com>
2019-06-17 23:32:59 -0300

    TODO: finish Eff Matching

---
+++ src/EffMatching.v
@@ -0,0 +1,57 @@
+Require Import Coq.Lists.List.
+Import ListNotations.
+
+Require Import Coq.Bool.Bool Coq.Strings.Ascii.
+Local Open Scope char.
+
+Require Import AsciiStrings.
+
+(* Act II - Scene 0, Before Semirings *)
+
+Inductive regex : Type :=
+  | Eps : regex
+  | Sym : bool -> ascii -> regex
+  | Alt : regex -> regex -> regex
+  | Seq : regex -> regex -> regex
+  | Rep : regex -> regex.
+
+Fixpoint empty (x : regex) : bool :=
+  match x with
+    | Eps => true
+    | Sym _ _ => false
+    | Alt p q => empty p || empty q
+    | Seq p q => empty p && empty q
+    | Rep r => true
+  end.
+
+Fixpoint final (x : regex) : bool :=
+  match x with
+    | Eps => false
+    | Sym b _ => b
+    | Alt p q => final p || final q
+    | Seq p q => final p && final q
+    | Rep r => final r
+  end.
+
+Fixpoint shift (m : bool) (x : regex) (c : ascii)
+  : regex :=
+  match x with
+    | Eps => Eps
+    | Sym _ x =>
+      Sym (m && eq_ascii x c) x
+    | Alt p q =>
+      Alt (shift m p c) (shift m p c)
+    | Seq p q =>
+      Seq (shift m p c)
+          (shift (m && empty p || final q) q c)
+    | Rep r =>
+      Rep (shift (m || final r) r c)
+  end.
+
+(* TODO fix rmatch *)
+Definition rmatch (r : regex) (s : string) : bool :=
+  match s with
+    | [] => empty r
+    | (c :: cs) =>
+      final (fold_left (shift false) (shift true r c) cs)
+  end.
---
+++ src/EffMatchingW.v
@@ -0,0 +1,37 @@
+Require Import Coq.Lists.List.
+Import ListNotations.
+
+Require Import Coq.Bool.Bool Coq.Strings.Ascii.
+Local Open Scope char.
+
+Require Import Semiring.
+
+(* Act II - Scene I, with Semirings *)
+
+Inductive Re_w c s : Type :=
+  | Eps_w : Re_w c s
+  | Sym_w : c -> s -> (Re_w c s)
+  | Alt_w : (Re_w c s) -> (Re_w c s) -> (Re_w c s)
+  | Seq_w : (Re_w c s) -> (Re_w c s) -> (Re_w c s)
+  | Rep_w : (Re_w c s) -> (Re_w c s).
+
+Record Reg_w
+  {A : Type} {zero : A} {one : A}
+  {add : A -> A -> A} {mul : A -> A -> A}
+  (s : Semiring zero one add mul) (c : A) :=
+  {
+    empty_w : s;
+    final_w : s;
+    reg_w : Re_w c s;
+  }.
+
+Definition eps_w {A : Type}
+  {zero : A} {one : A}
+  {add : A -> A -> A} {mul : A -> A -> A}
+  {s : Semiring zero one add mul}
+  : Reg_w c s :=
+  {|
+    empty_w := s.(one);
+    final_w := s.(zero);
+    reg_w := Eps_w c s;
+  |}.
